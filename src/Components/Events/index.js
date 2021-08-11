e9aa6aee62ad8704a8603c947e58733170791ca9
JuliaJu93 <kashlewa.ju93@gmail.com>
2021-08-11 22:23:39 +0300

    Added timeout

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Event } from '../Event';
 import { dateFormatting } from  './helpers/datesFormatting';
 import  { defineStyles } from './helpers/defineStyles';
 import  { sortingData } from './helpers/sortingData';
 
 export const Events = ({ eventsData, getEventWrapper }) => {
-    const currentDate = new Date();
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    useEffect(() => {
+        const interval = () => setInterval(() => setCurrentDate(new Date()), 60000);
+        interval();
+        return clearInterval(interval());
+    }, []);
+
     sortingData(eventsData);
     return <ul>{
         eventsData.map(event => {
